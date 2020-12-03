71447f426999073758606562af10295b0dbd6ee4
B-eck37 <brent.eckert7@gmail.com>
2020-12-02 23:45:16 -0700

    fix: updated styles, removed header, full keys

--- src/components/Calculators/BasicCalculator.tsx
+++ src/components/Calculators/BasicCalculator.tsx
@@ -6,31 +6,46 @@ const BasicCalculator = () => {
 
     
     return (
-        <div className="inner-calculator  basic">
+        <div className="basic">
+            <div className="screen-container">
+                <div className="screen-top">
+                    <div className="logo"></div>
+                    <div className="solar"></div>
+                </div>
             <div className="screen">
                 {total}
             </div>
+            </div>
+            <div className="middle-skinny"></div>
             <div className="basic-keys-container">
-            <div className="basic-keys basic-keyboard">
-                <button className="basic-key one">1</button>
-                <button className="basic-key two">2</button>
-                <button className="basic-key three">3</button>
-                <button className="basic-key 4">4</button>
-                <button className="basic-key 5">5</button>
-                <button className="basic-key 6 ">6</button>
-                <button className="basic-key 7">7</button>
-                <button className="basic-key 8">8</button>
+                <button className="basic-key 9">MRC</button>
+                <button className="basic-key 9">M-</button>
+                <button className="basic-key 9">M+</button>
+
+
                 <button className="basic-key 9">9</button>
+                <button className="basic-key 8">8</button>
+                <button className="basic-key 7">7</button>
+                <button className="basic-key 6 ">6</button>
+                <button className="basic-key 5">5</button>
+                <button className="basic-key 4">4</button>
+                <button className="basic-key three">3</button>
+                <button className="basic-key two">2</button>
+                <button className="basic-key one">1</button>
                 <button className="basic-key 0">0</button>
-                <button className="basic-key 00">00</button>
                 <button className="basic-key dot">.</button>
-            </div>
-            <div className="basic-keys basic-arithmetic">
-                <button className="basic-key plus">+</button>
-                <button className="basic-key minus"> - </button>
-                <button className="basic-key times">*</button>
-                <button className="basic-key divide">/</button>
-            </div>
+                <button className="basic-key clear">{"="}</button>
+
+                <button className="operator-key divide">÷</button>
+                <button className="operator-key times">x</button>
+                <button className="operator-key minus"> - </button>
+                <button className="operator-key plus">+</button>
+
+                <button className="operator-key divide">+/-</button>
+                <button className="operator-key divide">%</button>
+                <button className="operator-key divide">√</button>
+                <button className="basic-key equals">CE</button>
+                <button className="basic-key divide">On/C</button>
             </div>
         </div>
     )
