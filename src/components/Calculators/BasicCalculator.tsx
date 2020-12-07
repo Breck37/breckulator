538ef9121400e84ae0053085127f435597662c54
B-eck37 <brent.eckert7@gmail.com>
2020-12-07 10:25:40 -0700

    chore: organizing, added initial method defs, styles WIP

--- src/components/Calculators/BasicCalculator.tsx
+++ src/components/Calculators/BasicCalculator.tsx
@@ -2,14 +2,27 @@ import React, { useState } from 'react'
 import '../../styles/calculator.scss';
 
 const BasicCalculator = () => {
-    const [total, setTotal] = useState<string | number>(0)
+    const [total, setTotal] = useState<string | number | any>(0);
+
+
+    const updateTotal = (total: any) => {
+        setTotal(Number(total))
+        console.log(total)
+    }
+
+    const includeOperator = (operator: React.MouseEvent<HTMLElement>) => {
+        console.log(operator);
+    }
 
     
     return (
         <div className="basic">
             <div className="screen-container">
                 <div className="screen-top">
-                    <div className="logo"></div>
+                    <div className="logo">
+                        <p>Beckulator</p>
+                        <h4>7000</h4>
+                    </div>
                     <div className="solar"></div>
                 </div>
             <div className="screen">
@@ -18,34 +31,35 @@ const BasicCalculator = () => {
             </div>
             <div className="middle-skinny"></div>
             <div className="basic-keys-container">
-                <button className="basic-key 9">MRC</button>
-                <button className="basic-key 9">M-</button>
-                <button className="basic-key 9">M+</button>
+                <button onClick={e => updateTotal(e.target)} value="invert" className="operator-key divide">+/-</button>
+                <button onClick={e => updateTotal(e.target)} value="percent" className="operator-key divide">%</button>
+                <button onClick={e => updateTotal(e.target)} value="square" className="operator-key divide">√</button>
+                <button onClick={e => updateTotal(e.target)} value="clearEntry" className="basic-key equals">CE</button>
+                <button onClick={e => updateTotal(e.target)} value="poer" className="basic-key divide">On/C</button>
+
+                <button onClick={e => updateTotal(e.target)} value="" className="basic-key 9">MRC</button>
+                <button onClick={e => updateTotal(e.target)} value="" className="basic-key 9">M-</button>
+                <button onClick={e => updateTotal(e.target)} value="" className="basic-key 9">M+</button>
+                <button onClick={includeOperator} value="" className="operator-key divide">÷</button>
 
 
-                <button className="basic-key 9">9</button>
-                <button className="basic-key 8">8</button>
-                <button className="basic-key 7">7</button>
-                <button className="basic-key 6 ">6</button>
-                <button className="basic-key 5">5</button>
-                <button className="basic-key 4">4</button>
-                <button className="basic-key three">3</button>
-                <button className="basic-key two">2</button>
-                <button className="basic-key one">1</button>
-                <button className="basic-key 0">0</button>
-                <button className="basic-key dot">.</button>
-                <button className="basic-key clear">{"="}</button>
+                <button onClick={e => updateTotal(e.target)}   className="basic-key 9">9</button>
+                <button onClick={e => updateTotal(e.target)}   className="basic-key 8">8</button>
+                <button onClick={e => updateTotal(e.target)}   className="basic-key 7">7</button>
+                <button onClick={includeOperator} value="multiply" className="operator-key times">x</button>
 
-                <button className="operator-key divide">÷</button>
-                <button className="operator-key times">x</button>
-                <button className="operator-key minus"> - </button>
-                <button className="operator-key plus">+</button>
+                <button onClick={e => updateTotal(e.target)}   className="basic-key 6 ">6</button>
+                <button onClick={e => updateTotal(e.target)}   className="basic-key 5">5</button>
+                <button onClick={e => updateTotal(e.target)}   className="basic-key 4">4</button>
+                <button onClick={includeOperator} value="" className="operator-key minus"> - </button>
+                <button onClick={e => updateTotal(e.target)}   className="basic-key three">3</button>
+                <button onClick={e => updateTotal(e.target)}   className="basic-key two">2</button>
+                <button onClick={e => updateTotal(e.target)}   className="basic-key one">1</button>
+                <button onClick={e => updateTotal(e.target)}   className="basic-key 0">0</button>
+                <button onClick={e => updateTotal(e.target)}   className="basic-key dot">.</button>
+                <button onClick={includeOperator}   className="basic-key clear">{"="}</button>
+                <button onClick={includeOperator} value="" className="operator-key plus">+</button>
 
-                <button className="operator-key divide">+/-</button>
-                <button className="operator-key divide">%</button>
-                <button className="operator-key divide">√</button>
-                <button className="basic-key equals">CE</button>
-                <button className="basic-key divide">On/C</button>
             </div>
         </div>
     )
